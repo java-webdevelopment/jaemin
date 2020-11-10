0ad75c120aa901cb769a1ade1a80ec612f247bbe
NucPoop <nucpoop@Jam-MacBookPro.local>
2020-11-10 13:14:10 +0900

    [add] project

---
+++ banking/src/com/nucpoop/banking/BankingApplication.java
@@ -0,0 +1,17 @@
+package com.nucpoop.banking;
+
+import com.nucpoop.banking.manager.DBManager;
+import com.nucpoop.banking.manager.LoginManager;
+
+public class BankingApplication {
+
+	public static void main(String[] args) {
+		String id = "";
+		
+		DBManager.getInstance().init();
+		id = LoginManager.getInstance().process();
+		
+		System.out.println("[ USER : " + id + " ]");
+		System.out.println();
+	}
+}
---
+++ banking/src/com/nucpoop/banking/LoginState.java
@@ -0,0 +1,9 @@
+package com.nucpoop.banking;
+
+public enum LoginState {
+	OFFLINE,
+	SUCCESS,
+	INCORRECT_PASSWORD,
+	NOT_FOUND_ID,
+	ERROR
+}
